43194370bafe38a80943531acf36b3ebafb8a23c
Pravin Kumar <pravin.kumar08@sap.com>
2024-05-28 19:13:37 +0530

    dashboard and modules upgrade

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { MatDialog } from '@angular/material/dialog';
 import { PopupComponent } from '../../components/popup/popup.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenuComponent, PopupComponent],
+  imports: [MenuComponent, PopupComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  username: string = 'ChadarmoD';
+  isUpload: boolean = false;
+
+  tickets: any[] = [
+    {name: 'x', img: 'https://assets.codepen.io/285131/github.svg', desc: 'lajdflas'},
+    {name: 'y', img: 'https://xyz.com/img1.png', desc: 'lajdflas'},
+    {name: 'z', img: 'https://xyz.com/img1.png', desc: 'lajdflas'},
+  ];
+
   constructor(private dialogRef: MatDialog){}
 
   openDialog() {
@@ -19,6 +30,16 @@ export class DashboardComponent {
 
   closeDialog() {
     this.dialogRef.closeAll();
+  }
+
+  getTickets(category: string) {
+    console.log(category);
+    return this.tickets;
+  }
+
+  uploadTicket() {
+    this.isUpload = true;
+    console.log('uploading ticket');
   }
 
 }
