8c606b447c907e53f4a7fe46c386c24eb2d7fe50
Pravin Kumar <pravin.kumar08@sap.com>
2024-12-06 17:37:02 +0530

    https requests

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    BACKEND_URL: 'http://tradetix.api.ac2d4f6.kyma.ondemand.com',
+    BACKEND_URL: 'https://tradetix.api.ac2d4f6.kyma.ondemand.com',
     RESTFUL_URL: 'http://tradetix.pay.api.ac2d4f6.kyma.ondemand.com',
     BLOCKCHAIN_SERVER_URL: 'blockchain.api.ac2d4f6.kyma.ondemand.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    BACKEND_URL: 'http://tradetix.api.ac2d4f6.kyma.ondemand.com',
+    BACKEND_URL: 'https://tradetix.api.ac2d4f6.kyma.ondemand.com',
     RESTFUL_URL: 'http://tradetix.pay.api.ac2d4f6.kyma.ondemand.com',
     BLOCKCHAIN_SERVER_URL: 'blockchain.api.ac2d4f6.kyma.ondemand.com'
 };
